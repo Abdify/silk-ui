f107c5bac6d049f9b3aba15fb21121ddf66e3405
Abdify <67672693+Abdify@users.noreply.github.com>
2023-08-15 15:46:16 +0600

    testing lint staged pre commit hook

--- lint-staged.config.cjs
+++
@@ -1,8 +0,0 @@
-/* eslint-env node */
-module.exports = {
-  '*.js': ['eslint --fix', 'prettier --write', 'git add'],
-  '*.jsx': ['eslint --fix', 'prettier --write', 'git add'],
-  '*.ts': ['eslint --fix', 'prettier --write', 'git add'],
-  '*.tsx': ['eslint --fix', 'prettier --write', 'git add'],
-  '*.css': ['prettier --write', 'git add'],
-};
